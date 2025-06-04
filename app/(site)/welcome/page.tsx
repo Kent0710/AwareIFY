99c90dc8c53d51547e3530badc921fa6e69ee241
Kent0710 <christiankent001@gmail.com>
2025-06-04 15:52:11 +0800

    added button for linking in welcome page

--- app/(site)/welcome/page.tsx
+++ app/(site)/welcome/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default async function WelcomePage() {
     return (
         <div className="h-[100dvh] flex justify-center items-center">
-            Development..., Come back later.
+            <Link href={'/home'}>
+                <Button variant={'special'}>
+                    Test prototype
+                </Button>
+            </Link>
         </div>
     )
 }
